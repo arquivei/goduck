b9bb05e0acd9f6fd12ad2d3a4b667078a102ea59
André Missaglia <andre.missaglia@arquivei.com.br>
2021-03-22 14:09:34 -0300

    Removes unused code

--- impl/implqueue/message.go
+++ impl/implqueue/message.go
@@ -1,15 +1,5 @@
 package implqueue
 
-import "cloud.google.com/go/pubsub"
-
-type rawMessage struct {
-	msg *pubsub.Message
-}
-
-func (r rawMessage) Bytes() []byte {
-	return r.msg.Data
-}
-
 type mockRawMessage struct {
 	data []byte
 	idx  int
